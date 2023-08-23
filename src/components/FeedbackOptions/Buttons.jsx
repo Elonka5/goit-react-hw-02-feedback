4a0aa09c2fb5a7af55e68d62639ecba7d5fdbe4a
Elonka5 <101475428+Elonka5@users.noreply.github.com>
2023-08-23 14:26:32 +0300

    add changes

--- src/components/FeedbackOptions/Buttons.jsx
+++ src/components/FeedbackOptions/Buttons.jsx
@@ -30,38 +30,3 @@ FeedbackOptions.propTypes = {
   onLeaveFeedback: PropTypes.func.isRequired,
   options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
-
-// import PropTypes from 'prop-types';
-// import { ButtonsStyled } from 'components/FeedbackOptions/Buttons.styled';
-// import { Section } from 'components/Section/Section';
-// import { HiOutlineHeart } from 'react-icons/hi';
-// import { MdThumbDownOffAlt, MdSentimentNeutral } from 'react-icons/md';
-
-// const iconsArr = {
-//   good: <HiOutlineHeart size={22} />,
-//   neutral: <MdSentimentNeutral size={22} />,
-//   bad: <MdThumbDownOffAlt size={22} />,
-// };
-
-// export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-//   return (
-//     <Section title="Please leave feedback">
-//       {options.map(option => (
-//         <ButtonsStyled
-//           key={option}
-//           type="button"
-//           onClick={() => onLeaveFeedback(option)}
-//         >
-//           {iconsArr[option]}
-//           {option}
-//         </ButtonsStyled>
-//       ))}
-//     </Section>
-//   );
-// };
-
-// FeedbackOptions.propTypes = {
-//   onLeaveFeedback: PropTypes.func.isRequired,
-//   options: PropTypes.arrayOf(PropTypes.oneOf(['good', 'neutral', 'bad']))
-//     .isRequired,
-// };
